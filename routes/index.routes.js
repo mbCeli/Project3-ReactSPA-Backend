f615d76909f84e5233372755c553e7e762d06d74
mbCeli <munbaporicelina@outlook.com>
2025-03-05 10:51:14 +0000

    update 1.0

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const userRoutes = require("./user.routes");
-const gameRoutes = require("./game.routes");
-const playAnalyticsRoutes = require("./playAnalytics.routes");
+/* const gameRoutes = require("./game.routes");
+const playAnalyticsRoutes = require("./playAnalytics.routes"); */
 
 router.use("/users", userRoutes);
-router.use("/games", gameRoutes);
-router.use("/analytics", playAnalyticsRoutes);
+// router.use("/games", gameRoutes);
+// router.use("/analytics", playAnalyticsRoutes);
 
 module.exports = router;
