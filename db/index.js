853d0304883c81a7527a4d8890835ab5b9f7a610
mbCeli <munbaporicelina@outlook.com>
2025-03-05 11:15:10 +0000

    .env updated with drivers MONGODB_URI link

--- db/index.js
+++ db/index.js
@@ -2,12 +2,13 @@ const mongoose = require("mongoose");
 
 require("dotenv").config();
 
-const MONGO_URI = process.env.MONGODB_URI;
-
+const MONGO_URI =
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/games-app";
 mongoose
   .connect(MONGO_URI)
-  .then(() => {
-    console.log(`Connected to MongoDB`);
+  .then((x) => {
+    const dbName = x.connections[0].name;
+    console.log(`Connected to Mongo! Database name: "${dbName}"`);
   })
   .catch((err) => {
     console.error("Error connecting to mongo: ", err);
